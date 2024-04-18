a4495caca60ad682fc3e328096fed556f9e85632
krishna bisht <krishnabisht2003@gmail.com>
2024-04-18 23:20:20 +0530

    modification in landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import  React  from 'react'
 
 import './App.css'
 
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Hello World!n
     </h1>
   )
 }
