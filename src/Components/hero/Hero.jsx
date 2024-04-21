cacf18e04f6d6150154478a737422449d59743a9
rishabh-318 <rishu2nd@gmail.com>
2024-04-21 23:51:25 +0530

    Added custom button and Hero page

--- src/Components/hero/Hero.jsx
+++ src/Components/hero/Hero.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import heroImg from '../../assets/hero.png'
+import React from "react";
+import heroImg from "../../assets/hero.png";
+import Button from "../btn";
 
 function Hero() {
   return (
-    <div className='h-screen bg-center color-[#ffffff] bg-gradient-to-r from-blue-500 to-purple-500 bg-cover' style={{ backgroundImage: `linear-gradient(rgba(8,0,58,0.7),rgba(8,0,58,0.7)), url(${heroImg})` }}>
-        <div className=''>
-
-          
+    <div
+      className="h-screen bg-center text-[#ffffff] bg-gradient-to-r from-blue-500 to-purple-500 bg-cover flex"
+      style={{
+        backgroundImage: `linear-gradient(rgba(8,0,58,0.7),rgba(8,0,58,0.7)), url(${heroImg})`,
+      }}
+    >
+      <div className="text-center max-w-[800px] font-sans items-center m-auto flex-col">
+        <h1 className=" text-6xl font-semibold">
+          We Ensure better education for a better world
+        </h1>
+        <p className="py-10">
+          Our cutting-edge curriculum is designed to empower students with the
+          knowledge, skills, and experiences needed to excel in the dynamic
+          field of education
+        </p>
+        <div className="exploreBtn flex justify-center ">
+          <Button children="Explore More " flagImg />
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
