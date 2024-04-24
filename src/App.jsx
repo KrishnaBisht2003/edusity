3e24f4b49709bae4e24901ea5ca1035c08c39b7c
krishna bisht <krishnabisht2003@gmail.com>
2024-04-24 20:10:41 +0530

    changes in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Navbar from './Components/navbar/Navbar'
 import Hero from './Components/hero/Hero'
 import Projects from './Components/projects/Projects'
+import Gallery from './Components/gallery/Gallery'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Navbar />
       <Hero/>
       <Projects/>
+      <Gallery/>
     </>
   )
 }
