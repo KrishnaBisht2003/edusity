cf6fbe9357dd34dee8fe614982c11e62f3d60efb
rishabh-318 <rishu2nd@gmail.com>
2024-04-27 23:45:18 +0530

    making about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import  React  from 'react'
+import React, { useState } from "react";
 
-import './App.css'
-import Navbar from './Components/navbar/Navbar'
-import Hero from './Components/hero/Hero'
-import Projects from './Components/projects/Projects'
-import Gallery from './Components/gallery/Gallery'
+import "./App.css";
+import Navbar from "./Components/navbar/Navbar";
+import Hero from "./Components/hero/Hero";
+import Projects from "./Components/projects/Projects";
+import Gallery from "./Components/gallery/Gallery";
+import About from "./Components/About/About";
+import VideoPlayer from "./Components/videoPlayer/videoPlayer";
 
 export default function App() {
+  const [isVideoVisible, setVideoVisible] = useState(false);
+  function toggleVideo() {
+    setVideoVisible(true);
+  }
   return (
     <>
       <Navbar />
-      <Hero/>
-      <Projects/>
-      <Gallery/>
+      <Hero />
+      <Projects />
+      <About toggleVideo={toggleVideo} />
+      <Gallery />
+      <VideoPlayer
+        isVideoVisible={isVideoVisible}
+        setVideoVisible={setVideoVisible}
+      />
     </>
-  )
+  );
 }
